7e3767c74e2f7aa561f4078708c89154eb393a16
PavlenkoS <rootlet@rambler.ru>
2017-09-25 00:12:45 +0300

    Initial commit

---
+++ core/src/com/rootlet/drop/Drop.java
@@ -0,0 +1,118 @@
+package com.rootlet.drop;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
+
+public class Drop extends ApplicationAdapter {
+	OrthographicCamera camera;
+	SpriteBatch batch;
+	Texture dropImage;
+	Texture bucketImage;
+	Sound dropSound;
+	Music rainMusic;
+	Rectangle bucket;
+	Vector3 touchPos;
+	Array<Rectangle> raindrops;
+	long lastDropTime;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		dropImage = new Texture("kaplya.png");
+		bucketImage = new Texture("vedro.png");
+
+		touchPos = new Vector3();
+
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 /2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
+
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+        for (Rectangle raindrop: raindrops) {
+            batch.draw(dropImage, raindrop.x, raindrop.y);
+        }
+        batch.end();
+
+		if (Gdx.input.isTouched()) {
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = (int) (touchPos.x - 64 / 2);
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 500 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 500 * Gdx.graphics.getDeltaTime();
+
+		if (bucket.x < 0 ) bucket.x = 0;
+		if (bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+		if (TimeUtils.nanoTime() - lastDropTime > 500000000) spawnRaindrop();
+
+		Iterator<Rectangle> iter = raindrops.iterator();
+		while (iter.hasNext()) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if (raindrop.y + 64 < 0) iter.remove();
+            if (raindrop.overlaps(bucket)) {
+                dropSound.play();
+                iter.remove();
+            }
+		}
+	}
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800 - 64);
+		raindrop.y = 480 - 64;
+		raindrop.height = 64;
+		raindrop.width = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
+	}
+	
+	@Override
+	public void dispose () {
+        super.dispose();
+		batch.dispose();
+        rainMusic.dispose();
+        dropSound.dispose();
+        dropImage.dispose();
+		bucketImage.dispose();
+	}
+}
---
+++ html/src/com/rootlet/drop/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.rootlet.drop.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.rootlet.drop.Drop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 400);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Drop();
+        }
+}
