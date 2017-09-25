1f06d72b586319043fcfe9816b1bdd0bc08b5c09
pavlenko <rootlet@rootlet.com>
2017-09-26 00:19:25 +0300

    Screen, Game  implements. Score added.

--- desktop/src/com/rootlet/drop/desktop/DesktopLauncher.java
+++ desktop/src/com/rootlet/drop/desktop/DesktopLauncher.java
@@ -3,11 +3,12 @@ package com.rootlet.drop.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.rootlet.drop.Drop;
+import com.rootlet.drop.GameScreen;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Drop";
+		config.title = "GameScreen";
 		config.width = 800;
 		config.height = 480;
 		new LwjglApplication(new Drop(), config);
--- html/src/com/rootlet/drop/client/HtmlLauncher.java
+++ html/src/com/rootlet/drop/client/HtmlLauncher.java
@@ -3,7 +3,7 @@ package com.rootlet.drop.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.rootlet.drop.Drop;
+import com.rootlet.drop.GameScreen;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -14,6 +14,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new Drop();
+                return new GameScreen();
         }
 }
